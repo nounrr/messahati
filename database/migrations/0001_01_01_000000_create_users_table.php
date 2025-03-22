c22f96a38f1039c45dabf148e866c40fd689f086
nissrin mahan <nissrinmahan02@gmail.com>
2025-03-22 21:21:29 +0000

    les roles et les permission

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,12 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('cin')->unique();
+            $table->string('prenom');
+            $table->string('telephone');
+            $table->string('adresse');
+            $table->date('date_inscription');
+            $table->unsignedBigInteger('mutuel_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
