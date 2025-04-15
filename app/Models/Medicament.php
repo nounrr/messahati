c03027efed52809ba6ab26fdb5cb3e17b8327162
ayaziad <ayaziad387@gmail.com>
2025-04-15 23:34:01 +0100

    medicament

--- app/Models/Medicament.php
+++ app/Models/Medicament.php
@@ -6,13 +6,39 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medicament extends Model
 {
-    protected $fillable = ['nom_medicament','quantite','date_expiration','prix_unitaire','img_path'];
+    protected $fillable = [
+        'nom_medicament',
+        'dosage',
+        'forme',
+        'substance_active',
+        'classe_therapeutique',
+        'conditionnement',
+        'code_cip',
+        'laboratoire',
+        'indications',
+        'contre_indications',
+        'effets_indesirables',
+        'interactions',
+        'precautions',
+        'posologie',
+        'mode_administration',
+        'surdosage',
+        'conservation',
+        'prix',
+        'quantite',
+        'date_expiration',
+        'typemedicaments_id',
+        'img_path',
+    ];
 
-    public function ordonances(){
+    public function ordonances()
+    {
         return $this->belongsToMany(Ordonance::class);
     }
-    public function typemedicament(){
+
+    public function typemedicament()
+    {
         return $this->belongsTo(TypeMedicament::class);
     }
-
 }
+
