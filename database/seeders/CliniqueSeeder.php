cb7287042d26c3ea8de68a1d2017630a9ed2e26c
nissrin <nissrinmahan02@gmail.com>
2025-04-16 19:51:58 +0100

    test

---
+++ database/seeders/CliniqueSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CliniqueSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('cliniques')->insert([
+            [
+                'nom' => 'Clinique Principale',
+                'adresse' => '123 Rue Principale',
+                'email' => 'contact@clinique.com',
+                'description' => 'Clinique principale du système',
+                'site_web' => 'www.clinique.com',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        ]);
+    }
+} 
